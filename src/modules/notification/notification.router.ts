f12774005e804296bc263dec6ff535e4a6d4441e
Sajjad177 <sajjadhossainx06@gmail.com>
2026-04-29 02:52:13 +0600

    get shopkeeper and user notification api added

--- src/modules/notification/notification.router.ts
+++ src/modules/notification/notification.router.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
-import { getAllNotifications, markAllAsRead } from "./notification.controller";
+import { getAllNotificationByUser, getAllNotifications, getShopkeeperAllNotifications, markAllAsRead } from "./notification.controller";
 import { protect } from "../../middlewares/auth.middleware";
 
 const router = Router();
 
 router.get('/', protect, getAllNotifications);
+router.get("/shopkeeper", protect, getShopkeeperAllNotifications);
+router.get("/user", protect, getAllNotificationByUser);
+
 
 router.patch('/read/all', markAllAsRead);
+
 
 const notificationRouter = router;
 export default notificationRouter;
